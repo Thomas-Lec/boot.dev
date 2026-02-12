1c9b289d39c7db2c200fa5bfbb6cc0b8c88035a7
Thomas-Lec <tlechner246@gmail.com>
2026-02-12 20:03:31 +0100

    12022026_complete

--- functional_programming/ch5_L7_Function/main.py
+++ functional_programming/ch5_L7_Function/main.py
@@ -1,6 +1,12 @@
 def get_filter_cmd(filter_one, filter_two):
-    def filter_cmd(content, option):
-        pass
+    def filter_cmd(content, option="--one"):
+        if option in {"--one"}:
+            return filter_one(content)
+        if option in {"--two"}:
+            return filter_two(content)
+        if option in {"--three"}:
+            return filter_two(filter_one(content))
+        raise Exception("invalid option")
 
     return filter_cmd
 
