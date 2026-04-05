344367e6184b084e65e021190c269c913775903e
Thomas-Lec <tlechner246@gmail.com>
2026-04-05 22:05:30 +0200

    solved_CH7_Currying_L6_Currying_Practice

--- functional_programming/CH7/L6_Currying_Practice/main.py
+++ functional_programming/CH7/L6_Currying_Practice/main.py
@@ -1,2 +1,15 @@
 def new_resizer(max_width, max_height):
-    pass
+
+    def inner_resizer(min_width=0,min_height=0):
+        if min_width > max_width or min_height > max_height:
+            raise Exception ("minimum size cannot exceed maximum size")
+
+        def innermost_inner_resizer(width, height):
+            width = min(width,max_width)
+            width = max(width,min_width)
+            height = min(height,max_height)
+            height = max(height,min_height)
+            return width,height
+        return innermost_inner_resizer
+    return inner_resizer
+
