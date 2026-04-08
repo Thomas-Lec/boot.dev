3760cf991ffbaada50495f4e1d806ab0cbb25339
Thomas-Lec <tlechner246@gmail.com>
2026-04-08 20:29:36 +0200

    08042026_CH8_L2_Args_and_Kwargs

---
+++ functional_programming/CH8/L2_Args_and_Kwargs/logger.py
@@ -0,0 +1,11 @@
+def args_logger(*args, **kwargs):
+    counter = 1
+    for arg in args:
+        print(f"{counter}. {arg}")
+        counter +=1 
+
+
+    for key, value in sorted(kwargs.items()):
+        print("* {0}: {1}".format(key,value))
+        counter +=1 
+
