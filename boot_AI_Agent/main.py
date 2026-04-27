ba32d95d7d9195b40bf34b2b375f6659e65dc54e
Thomas-Lec <tlechner246@gmail.com>
2026-04-27 20:38:55 +0200

    correct structure

--- boot_AI_Agent/main.py
+++ boot_AI_Agent/main.py
@@ -1,41 +1,44 @@
-import os, argparse
+import argparse
+import os
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-#load the environment API Key + get the connection
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
-#add a parser for user entry
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str,help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-
-#check if API key is valid
-if api_key == None:
-    raise RuntimeError("API Key not found")
-
 
 def main():
-    response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages
-    )
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    if response.usage_metadata == None:
-        raise RuntimeError("failed API request")
-
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"User prompt: {args.user_prompt}\n")
 
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
