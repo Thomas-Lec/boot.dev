52515f8a244bb93e3355bd98b0d48fbc06dcdfab
Thomas-Lec <tlechner246@gmail.com>
2026-04-29 21:12:52 +0200

    commit changes based on boots - working on loop

--- boot_AI_Agent/test_get_files_info.py
+++ boot_AI_Agent/test_get_files_info.py
@@ -1,6 +1,6 @@
 import os
 
-def get_files_info(working_directory, directory="."):
+def get_files_info("calculator", directory="."):
     working_dir_abs = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_dir_abs,directory))
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
@@ -8,15 +8,15 @@ def get_files_info(working_directory, directory="."):
     if valid_target_dir == False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if os.path.isdir(target_dir) == False:
-        return f'Error: "{target_dir}" is not a directory'
+    if os.path.isdir(working_dir_abs) == False:
+        return f'Error: "{working_dir_abs}" is not a directory'
 
     try:
 
-        for target_dir_item in os.listdir(target_dir):
+        for target_dir_item in target_dir:
             file_size = os.path.getsize(target_dir_item)
             is_dir = os.path.isdir(target_dir_item)
-            return f"- {target_dir_item}: file_size={file_size} bytes, is_dir={isdir}"
+        return f"- {target_dir_item}: file_size={file_size} bytes, is_dir={isdir}"
 
     except Exception as e:
         return f"Error: unexpected Error occured {e}" 
