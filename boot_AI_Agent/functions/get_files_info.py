c5b9a241dee01c904f7d7b5f5a77e23ab2bf4835
Thomas-Lec <tlechner246@gmail.com>
2026-05-02 18:08:13 +0200

    optimize CH2_L2

--- boot_AI_Agent/functions/get_files_info.py
+++ boot_AI_Agent/functions/get_files_info.py
@@ -1,25 +1,22 @@
 import os
 
+
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs,directory))
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-
-    if valid_target_dir == False:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-    if os.path.isdir(target_dir) == False:
-        return f'Error: "{target_dir}" is not a directory'
-
     try:
-
-        results = []
-        for target_dir_item in os.listdir(target_dir):
-            file_size = os.path.getsize(os.path.join(target_dir, target_dir_item))
-            is_dir = os.path.isdir(os.path.join(target_dir, target_dir_item))
-            results.append(f"- {target_dir_item}: file_size={file_size} bytes, is_dir={is_dir}")
-        return "\n".join(results)
-
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+        if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
-        return f"Error: unexpected Error occured {e}" 
-
+        return f"Error listing files: {e}"
